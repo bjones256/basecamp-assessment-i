7cd37f4f7864fdcedd5ac6eea7451192d92cf761
Brian Jones <BriansMac@Brians-MacBook-Pro.local>
2018-10-02 09:04:47 -0500

    final commit

--- assessment.js
+++ assessment.js
@@ -17,12 +17,13 @@ var me = "My name is "+myName+" and my favorite number is " +faveNum;
 // #5 Using the previously created faveNum variable, create a new variable called 'trueFaveNum'. *trueFaveNum* should equal true when comparing faveNum to your favorite number.v // HINT: Use a comparison operator
 
 var trueFaveNum = trueFaveNum != faveNum;
-console.log(trueFaveNum,faveNum);
+
 // #6 Create a new variable called notHakeem. Using the previously created myName variable, test that your name DOES NOT equal the string 'Hakeem Olajuwon'. If your name does happen to be Hakeem Olajuwon, test against 'Karl Malone'
 
 var notHakeem = myName != "Hakeem Olajuwon";
 
 // #7 Create an if statement that checks the age variable below. If age is greater than 16, then set canDrive to true otherwise set canDrive to false. The canDrive variable will be set by the unit test so do not adjust it.
+
 var age = 18;
 var canDrive;
 if(age >= 16){
@@ -60,4 +61,3 @@ function greeter(name,number){
   return "Hi! I am " + name +" and I am "+number+" years old";
 }
 greeter("Brian",38);
-console.log(me,faveNum,trueFaveNum);
